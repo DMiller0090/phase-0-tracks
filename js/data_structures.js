2a90b66051939c06d34a912537bab0ffc1cc3da9
Dereck Miller <pinhigh1@gmail.com>
2016-11-15 18:03:23 -0600

    Finish 7.1

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,5 @@
-var colors = ["blue","green","purple","orange"]
-var names = ["Ed","Misty","Brownie","Pharaoh"]
+var colors = ["blue","green","purple","orange"];
+var names = ["Ed","Misty","Brownie","Pharaoh"];
 
 colors.push("yellow");
 names.push("Chestnut");
@@ -8,4 +8,27 @@ var horses = {};
 for(var i = 0; i < colors.length;i++){
 	horses[names[i] ] = colors[i];
 }	
-console.log(horses)
+console.log(horses);
+
+function Car(model,year,color){
+	console.log("New car:", this);
+	this.model = model;
+	this.year = year;
+	this.color = color;
+
+	this.accelerate = function(speed){
+		console.log("Now going " + speed + "mph.");
+	};
+}
+
+var carOne = new Car("Civic",2016,"Black");
+carOne.accelerate(250);
+console.log(carOne);
+
+var carTwo = new Car("CX5",2017,"Red");
+carTwo.accelerate(300);
+console.log(carTwo);
+
+var carThree = new Car("F150",2017,"White");
+carThree.accelerate(100);
+console.log(carThree);
